e1eba3422b5b97e72b3fb2c2c19e725ed04326c4
Fruha <57077738+Fruha@users.noreply.github.com>
2020-10-07 22:16:23 +0300

    Add files via upload

---
+++ 3 sem/3 laba/a/Card.cpp
@@ -0,0 +1,200 @@
+﻿#include "Card.h"
+#include <string> 
+#include <iostream>
+
+using namespace std;
+
+namespace laba3
+{
+	Card::Card(bool a) 
+	{	
+		if (a) 
+		{
+			set_suit_int(rand() % 4 + 1);
+			set_rang_int(rand() % 13 + 2);
+		}
+		else 
+		{
+			suit_int = 0;
+			suit_ch  = '0';
+			rang_str = "0";
+			rang_int = 0;
+		}
+	}
+
+	Card::Card(char suit_ch_temp, string rang_str_temp)
+	{
+		set_suit_ch (suit_ch_temp);
+		set_rang_str(rang_str_temp);
+	}
+
+	Card::Card(char suit_ch_temp, int rang_int_temp)
+	{
+		set_suit_ch (suit_ch_temp);
+		set_rang_int(rang_int_temp);
+	}
+
+	Card::Card(int suit_int_temp, string rang_str_temp)
+	{
+		set_suit_int(suit_int_temp);
+		set_rang_str(rang_str_temp);
+	}
+
+	Card::Card(int suit_int_temp, int rang_int_temp)
+	{
+		set_suit_int(suit_int_temp);
+		set_rang_int(rang_int_temp);
+	}
+
+	void Card::set_suit_ch(char suit_ch_temp)
+	{
+		suit_int = suit_ch_to_int(suit_ch_temp);
+		suit_ch  = suit_ch_temp;
+	}
+
+	void Card::set_suit_int(int suit_int_temp)
+	{
+		suit_ch  = suit_int_to_ch(suit_int_temp);
+		suit_int = suit_int_temp;
+	}
+
+	void Card::set_rang_str(string rang_str_temp)
+	{
+		rang_int = rang_str_to_int(rang_str_temp);
+		rang_str = rang_str_temp;
+	}
+
+	void Card::set_rang_int(int rang_int_temp) 
+	{
+		rang_str = rang_int_to_str(rang_int_temp);
+		rang_int = rang_int_temp;
+	}
+	
+	int	Card::compare(const Card& card, bool cons_the_suit) const // -1 left more, 0 equal, 1 right more, 2 incomparable
+	{
+		if (this->suit_int != card.suit_int)
+		{
+			if (cons_the_suit == 1) {
+				if (this->suit_int < card.suit_int) {
+					return 1;
+				}
+				else 
+				{
+					return -1;
+				}
+			}
+			else 
+			{
+				return 2;
+			}
+			
+		}
+
+		if (this->rang_int < card.rang_int) 
+		{
+			return 1;
+		}
+		else if (this->rang_int == card.rang_int)
+		{
+			return 0;
+		} 
+		else if (this->rang_int > card.rang_int)
+		{
+			return -1;
+		}
+		throw("Error compare");
+	}
+
+	int Card::rang_str_to_int(string rang_str_temp) const
+	{
+		for (int i = 2; i < 11; i += 1) 
+		{
+			if (rang_str_temp == to_string(i)) 
+			{
+				return i;
+			}
+		}
+		if (rang_str_temp == "J")
+		{
+			return 11;
+		}
+		else if (rang_str_temp == "Q")
+		{
+			return 12;
+		}
+		else if (rang_str_temp == "K") 
+		{
+			return 13;
+		}
+		else if (rang_str_temp == "A") 
+		{
+			return 14;
+		}
+		throw("Error rang_str");
+	}
+
+	string Card::rang_int_to_str(int rang_int_temp) const
+	{
+		if (1 < rang_int_temp && rang_int_temp < 11) 
+		{
+			return to_string(rang_int_temp);
+		}
+		if (rang_int_temp == 11) 
+		{
+			return ("J");
+		}
+		else if (rang_int_temp == 12)
+		{
+			return ("Q");
+		}
+		else if (rang_int_temp == 13) 
+		{
+			return ("K");
+		}
+		else if (rang_int_temp == 14) 
+		{
+			return ("A");
+		}
+		throw("Error rang_int");
+	}
+
+	int Card::suit_ch_to_int(char suit_ch_temp) const
+	{
+		if (suit_ch_temp == 'S') {
+			return 1;
+		}
+		else if (suit_ch_temp == 'H')
+		{
+			return 2;
+		}
+		else if (suit_ch_temp == 'C')
+		{
+			return 3;
+		}
+		else if (suit_ch_temp == 'D')
+		{
+			return 4;
+		}
+		throw ("Error suit_ch");
+	}
+
+	char Card::suit_int_to_ch(int suit_int_temp) const
+	{
+		switch (suit_int_temp) 
+		{
+		case 1:
+			return 'S';
+		case 2:
+			return 'H';
+		case 3:
+			return 'C';
+		case 4:
+			return 'D';
+		}
+		throw ("Error suit_int");
+	}
+	void Card::print() const
+	{
+		cout << suit_ch << rang_str << endl;
+	}
+}
---
+++ 3 sem/3 laba/a/Deck.cpp
@@ -0,0 +1,147 @@
+#include "Deck.h"
+
+using namespace std;
+
+namespace laba3 
+{
+	Deck::Deck(int len_temp)
+	{
+		if (len_temp > 52 || len_temp < 0) 
+		{
+			throw("Error len");
+		}
+		len = 0;
+		for (int i = 0; i < len_temp; i += 1) {
+			add_new_random_card();
+		}
+	}
+
+	Deck::Deck(Card card)
+	{
+		this->len = 0;
+		this->data[0] = card;
+	}
+
+	int Deck::find_card(const Card& card) const
+	{
+		for (int i = 0; i < len; i += 1) {
+			if (data[i].compare(card) == 0) 
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	void Deck::set_card(Card card, int i) 
+	{
+		if (i < 0 || i > len) {
+			throw ("Error index");
+		}
+		data[i] = card;
+	}
+
+	int Deck::delete_card(const Card& card)
+	{
+		int s1 = find_card(card);
+		if (s1 == -1)
+		{
+			return 1;
+		}
+		for (int i = s1; i < len - 1; i += 1)
+		{
+			data[i] = data[i + 1];
+		}
+		len -= 1;
+		data[len] = Card(0);
+		return 0;
+	}
+	
+	int Deck::delete_card(int s1)
+	{
+		if (s1 < 0 || s1 >= len)
+		{
+			return 1;
+		}
+		for (int i = s1; i < len - 1; i += 1)
+		{
+			data[i] = data[i + 1];
+		}
+		len -= 1;
+		data[len] = Card(0);
+		return 0;
+	}
+
+
+	int Deck::add_new_random_card()
+	{
+		if (len == 52) {
+			return 1;
+		}
+		Card temp = Card(1);
+		while (find_card(temp) != -1)
+		{
+			temp = Card(1);
+		}
+
+		data[len] = temp;
+		len += 1;
+		return 0;
+	}
+
+	int Deck::add_new_card(Card card)
+	{
+		if (len == 52) {
+			return 1;
+		}
+		if (find_card(card) != -1) 
+		{
+			return 2;
+		}
+
+		data[len] = card;
+		len += 1;
+		return 0;
+	}
+
+	void Deck::swap(Card& a, Card& b)
+	{
+		Card temp = a;
+		a = b;
+		b = temp;
+	}
+
+	void Deck::sort(int start, int end) 
+	{
+		if (end > len - 1)
+		{
+			end = len - 1;
+		}
+		if (start < end) 
+		{
+			Card temp = data[start];
+			int s = start, e = end;
+			do 
+			{
+				while (data[s].compare(temp, 1) == 1) s += 1;
+				while (data[e].compare(temp, 1) == -1) e -= 1;
+				if (s <= e) 
+				{
+					swap(data[s], data[e]);
+					s += 1;
+					e -= 1;
+				}
+			} while (s <= e);
+			sort(start, e);
+			sort(s, end);
+		}
+		
+	}
+	void Deck::print() const
+	{
+		for (int i = 0; i < len; i += 1) 
+		{
+			data[i].print();
+		}
+	}
+}
